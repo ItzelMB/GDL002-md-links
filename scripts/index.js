ed207c16dc2904bca7a6306a47e5ad8f2dfebcd1
ItzelMB <it.mobec@gmail.com>
2019-05-01 16:50:41 -0500

    Add readPath test, pathExist function and test, everything functional

--- scripts/index.js
+++ scripts/index.js
@@ -1,76 +1,14 @@
 const fs = require('fs');
-//const path = require('path');
 
-
-
-/*
-module.exports = {
-
-  pathExist: function (pathfile) {
-    if(pathfile == undefined){
+//Verify existing path
+let pathExist = (currentPath) => {
+  if(currentPath == undefined){
+      console.log("Invalid or non existing path");
       return false;
-    } else {
+  } else {
       return true;
-    }
-  },
-
-  /*readText: function (mdFile) {
-    let lines = fs.readFileSync(path, 'utf-8').split('\n');
- // return true;
-  
-
-  links: function (path) {
-    let getLinkLines = new RegExp('\\[+.*\\]s*\\(+.*\\)');
-    let getLinks = new RegExp('\\(+.*\\)');
-    let arrayLinks = [];
-  
-    let lines = fs.readFileSync(path, 'utf-8').split('\n');
-    //console.log(lines);
-  
-    lines.forEach(line => {
-      let matches = getLinkLines.exec(line);
-      if (matches !== null) {
-        matches.forEach(match => {
-          let urls = getLinks.exec(match);
-          if (urls !== null) {
-            //console.log(urls);
-            urls.forEach(url => arrayLinks.push(url));
-          }
-        });
-      }
-    });
-  
-    console.log(arrayLinks);
-  },
-
-  readPath: function (directory) {
-    if (directory.includes('.md')) {
-      links(directory);
-    } else if (!directory.includes('.')) {
-      let userPath = fs.readdirSync(directory);
-      console.log(userPath);
-      if (userPath !== null) {
-        userPath.forEach(file => {
-          //console.log(file);
-          if (file.includes('.md')) {
-            links(directory + '\\' + file);
-          }
-        });
-      }
-    }
   }
-  //readPath('../README.md');
-
 };
-
-*/
-
-
-module.exports = {
-
-  
-};
-
 
 //Read file and get links in an array
 const links = (path) => {
@@ -88,77 +26,48 @@ const links = (path) => {
         let urls = getLinks.exec(match);
         if (urls !== null) {
           //console.log(urls);
-          urls.forEach(url => arrayLinks.push(url));
+          urls.forEach(url => arrayLinks.push(url.replace('(','').replace(')','')));
         }
       });
     }
   });
 
   console.log(arrayLinks);
+  return arrayLinks;
 };
 
 
 //Get from a directory or a file
 const readPath = (directory) => {
-  if (directory.includes('.md')) {
-    links(directory);
+  if (directory.includes('.')) {
+    if(directory.includes('.md')){
+      return links(directory);
+    }else{
+      console.log('This is not a md file');
+      return 'This is not a md file';
+    }
   } else if (!directory.includes('.')) {
     let userPath = fs.readdirSync(directory);
-    console.log(userPath);
+    //console.log(userPath);
     if (userPath !== null) {
       userPath.forEach(file => {
         //console.log(file);
         if (file.includes('.md')) {
-          links(directory + '\\' + file);
+          console.log(directory + '\\' + file);
+          return links(directory + '\\' + file);
         }
       });
     }
   }
 };
-readPath('../README.md');
+readPath('C:\\Users\\Itina\\Documents\\LABORATORIA\\Proyectos-GDL02\\Proyecto-05\\GDL002-md-links');
 
-
-//module
-/*module.exports = () => {
-  pathExist();
-  readPath();
-};*/
-
-
-/*mdLinks:  () => {
-    console.log('hello');
-  };*/
+module.exports = {
+  pathExist,
+  readPath
+};
 
 
 /*
-'C:\\Users\\Itina\\Documents\\LABORATORIA\\Proyectos-GDL02\\Proyecto-05\\GDL002-md-links'
-
-
-let instream = fs. createReadStream('../README.md');
-let outstream = new stream;
-let readlines = readline.createInterface(instream, outstream);
-let getLinkLines = new RegExp('\\[+.*\\]s*\\(+.*\\)');
-let getLinks = new RegExp('\\(+.*\\)')
-
-readlines.on('line',(line) => {
-  let matches = getLinkLines.exec(line);
-  if( matches !==null){
-    //console.log(matches);
-    matches.map(match=>{
-      let urls = getLinks.exec(match);
-      if ( urls !== null){
-        console.log(urls);
-      };
-    });
-  };
-});
-
-readlines.on('close', ()=>{
-  console.log('close')
-});*/
-
-/*fs.readFile ('C:\\Users\\Itina\\Documents\\LABORATORIA\\Proyectos-GDL02\\Proyecto-05\\GDL002-md-links\\README.md',
-(err, data)=> {
-  if (err) throw err;
-  console.log(data);
-});*/
+'C:\\Users\\Itina\\Documents\\LABORATORIA\\Proyectos-GDL02\\Proyecto-05\\GDL002-md-links\\README.md'
+*/
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,16 +1,10 @@
-const mdLinks = require('../scripts/index.js');
+//const mdLinks = require('../scripts/index.js');
 const index = require('../scripts/index.js');
+const linksArr = require('./mocks/links-array.json');
 
 
-
-describe('mdLinks', () => {
-  it('should be a function', () => {
-    expect(typeof(mdLinks)).toBe('function');
-  });
-});
-
-//Test existing path
-describe('pathExist', ()=> {
+//Exist path test
+describe('pathExist', () => {
   it('should have an existing path and return true', () => {
     expect(index.pathExist('../README.md')).toBe(true);
   });
@@ -20,8 +14,22 @@ describe('pathExist', ()=> {
   });
 });
 
+//Return links arr
 describe('readPath', () => {
-  it('should return a links array', () => {
-    expect(index.links('../HI.md')).toBe('[(https://www.figma.com/file/aIfh7zOWjTYdWvJKqgHAFcbk/Main-interaction?node-id=0%3A1),(https://www.figma.com/file/aIfh7zOWjTYdWvJKqgHAFcbk/Main-interaction?node-id=0%3A1)]');
+  it('should return a message: \'This is not a md file\'', () => {
+    expect(index.readPath('C:\\Users\\Itina\\Documents\\LABORATORIA\\Proyectos-GDL02\\Proyecto-05\\GDL002-md-links\\package.json')).toBe('This is not a md file');
+  });
+  
+  it('should return an array links', () => {
+    expect(index.readPath('C:\\Users\\Itina\\Documents\\LABORATORIA\\Proyectos-GDL02\\Proyecto-05\\GDL002-md-links\\README.md')).toEqual(linksArr.arr);
   });
 });
+
+
+/*
+describe('mdLinks', () => {
+  it('should be a function', () => {
+    expect(typeof(mdLinks)).toBe('function');
+  });
+});
+*/
